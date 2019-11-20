f7f5b6f334273a2f1ddc59757bd3f1292e61dc76
matheusleandroo <mattleandroo@outlook.com>
2019-11-20 10:17:09 -0200

    setting statusbar

--- src/routes.js
+++ src/routes.js
@@ -15,7 +15,7 @@ const Routes = createAppContainer(
       headerBackTitleVisible: false,
       defaultNavigationOptions: {
         headerStyle: {
-          backgroundColor: '#7159c1',
+          backgroundColor: '#191920',
         },
         headerTintColor: '#FFF',
       },
