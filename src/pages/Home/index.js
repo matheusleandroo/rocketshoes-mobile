6180906cee5e2f9b4fab5af9c27ac1d3dc142d58
Matheus Leandro <mattleandroo@outlook.com>
2019-12-02 20:54:54 -0300

    change layout home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,23 +1,43 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Text, Button, FlatList } from 'react-native';
+import { Button, FlatList } from 'react-native';
 
-import { Container, ProductList } from './styles';
+import {
+  Container,
+  ProductList,
+  ProductImage,
+  ProductTitle,
+  ProductPrice,
+  ButtonContainer,
+  ButtonIcon,
+  IconShoppingCart,
+  IconText,
+  ButtonText,
+} from './styles';
 
 export default class Home extends Component {
   state = {
     products: [
       {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        title: 'First Item',
+        id: 1,
+        title: 'Tênis de Caminhada Leve Confortável',
+        price: 179.9,
+        image:
+          'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg',
       },
       {
-        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-        title: 'Second Item',
+        id: 2,
+        title: 'Tênis VR Caminhada Confortável Detalhes Couro Masculino',
+        price: 139.9,
+        image:
+          'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg',
       },
       {
-        id: '58694a0f-3da1-471f-bd96-145571e29d72',
-        title: 'Third Item',
+        id: 3,
+        title: 'Tênis Adidas Duramo Lite 2.0',
+        price: 219.9,
+        image:
+          'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis3.jpg',
       },
     ],
   };
@@ -41,16 +61,22 @@ export default class Home extends Component {
         <FlatList
           horizontal
           data={products}
-          keyExtractor={product => product.id}
+          keyExtractor={product => product.id.toString()}
           renderItem={({ item }) => (
             <ProductList>
-              <Text>{item.title}</Text>
+              <ProductImage source={{ uri: item.image }} />
+              <ProductTitle>{item.title}</ProductTitle>
+              <ProductPrice>R$ {item.price}</ProductPrice>
+              <ButtonContainer>
+                <ButtonIcon>
+                  <IconShoppingCart name="shopping-cart" />
+                  <IconText>0</IconText>
+                </ButtonIcon>
+                <ButtonText>ADICIONAR</ButtonText>
+              </ButtonContainer>
             </ProductList>
           )}
         />
-        {/* <ProductList>
-          <Text>Teste</Text>
-        </ProductList> */}
         <Button
           title="Go to Card"
           onPress={() => {
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export const Container = styled.View`
   flex: 1;
@@ -7,10 +8,72 @@ export const Container = styled.View`
 `;
 
 export const ProductList = styled.View`
-  background: #eee;
-  border-radius: 5px;
-  padding: 15px;
-  margin: 0px 10px 20px 0px;
-  width: 210px;
-  height: 370px;
+  flex: 1;
+  background: #ffff;
+  border-radius: 4px;
+  padding: 20px;
+  margin: 0px 20px 20px 0px;
+  width: 280px;
+  height: 450px;
+`;
+
+export const ProductImage = styled.Image.attrs(props => ({
+  uri: props.source,
+}))`
+  flex: 0.7;
+  width: 240px;
+  height: 200px;
+  margin-bottom: 5%;
+`;
+
+export const ProductTitle = styled.Text`
+  flex: 0.3;
+  font-size: 18px;
+  line-height: 20px;
+  color: #333;
+  margin-top: 5px;
+`;
+
+export const ProductPrice = styled.Text`
+  flex: 0.1;
+  font-weight: bold;
+  font-size: 23px;
+  margin: 5px 0 20px;
+`;
+
+export const ButtonContainer = styled.View`
+  flex: 0.17;
+  flex-direction: row;
+  height: 10px;
+  background-color: #7159c1;
+  border-radius: 4px;
+  overflow: hidden;
+  align-items: center;
+`;
+
+export const ButtonIcon = styled.View`
+  flex: 0.2;
+  flex-direction: row;
+  background: rgba(0, 0, 0, 0.1);
+  align-items: center;
+  padding: 12px;
+`;
+
+export const IconShoppingCart = styled(Icon)`
+  font-size: 25px;
+  color: #ffff;
+`;
+
+export const IconText = styled.Text`
+  color: #ffff;
+  font-size: 18px;
+  padding-left: 5px;
+`;
+
+export const ButtonText = styled.Text`
+  flex: 0.75;
+  font-size: 20px;
+  font-weight: bold;
+  color: #ffff;
+  text-align: center;
 `;
