14c121256cc202194d77371c95d7519f17c0ca2b
Matheus Leandro <mattleandroo@outlook.com>
2019-12-03 22:34:55 -0300

    style header cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,32 +1,53 @@
 import React, { Component } from 'react';
 
-import { Container, CartList } from './styles';
-
-import { formatPrice } from '../../util/format';
-import api from '../../services/api';
+import { Text } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {
+  Container,
+  CartList,
+  ProductInfo,
+  ProductImage,
+  ProductDetails,
+  ProductTitle,
+  ProductPrice,
+  ProductTrash,
+  ProductOptions,
+} from './styles';
 
 export default class Cart extends Component {
   state = {
-    products: [],
+    products: [
+      {
+        id: 1,
+        title: 'Tênis de Caminhada Leve e muito Confortável',
+        price: 179.9,
+        image:
+          'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg',
+      },
+    ],
   };
-
-  async componentDidMount() {
-    const response = await api.get('products');
-
-    const data = response.data.map(product => ({
-      ...product,
-      priceFormatted: formatPrice(product.price),
-    }));
-
-    this.setState({ products: data });
-  }
 
   render() {
     const { products } = this.state;
 
     return (
       <Container>
-        <CartList />
+        <CartList>
+          <ProductInfo>
+            <ProductImage source={{ uri: products[0].image }} />
+            <ProductDetails>
+              <ProductTitle>{products[0].title}</ProductTitle>
+              <ProductPrice>R${products[0].price}0</ProductPrice>
+            </ProductDetails>
+            <ProductTrash>
+              <Icon name="delete-forever" color="#715c94" size={30} />
+            </ProductTrash>
+          </ProductInfo>
+          <ProductOptions>
+            <Text>Teste</Text>
+            <Text>Teste</Text>
+          </ProductOptions>
+        </CartList>
       </Container>
     );
   }
--- src/pages/Cart/styles.js
+++ src/pages/Cart/styles.js
@@ -4,11 +4,51 @@ export const Container = styled.View`
   flex: 1;
   padding: 15px;
   background: #191920;
+  align-items: center;
 `;
 
 export const CartList = styled.View`
-  flex: 1;
   background: #ffff;
   border-radius: 4px;
-  padding: 20px;
+  padding: 10px;
+  min-height: 320px;
+`;
+
+export const ProductInfo = styled.View`
+  flex-direction: row;
+`;
+
+export const ProductImage = styled.Image.attrs(props => ({
+  uri: props.source,
+}))`
+  width: 140px;
+  height: 110px;
+`;
+
+export const ProductDetails = styled.View`
+  padding: 10px;
+  width: 195px;
+`;
+
+export const ProductTitle = styled.Text`
+  font-size: 15px;
+`;
+
+export const ProductPrice = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+  padding-top: 10px;
+`;
+
+export const ProductTrash = styled.TouchableOpacity`
+  height: 24px;
+  width: 24px;
+  margin-top: 25px;
+`;
+
+export const ProductOptions = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: 20px;
 `;
